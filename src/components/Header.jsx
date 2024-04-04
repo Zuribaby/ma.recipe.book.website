b44167e1b292957ae018e21767d81328218a269d
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-04-04 15:43:30 +0200

    addded styling to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
-// Header.js
 import React from "react";
+import logo from "../images/logo.png";
+import "../css/Header.css";
 
 function Header() {
   return (
     <header className="header">
+      <img src={logo} alt="Logo" className="logo" />
       <h1>My Website Header</h1>
     </header>
   );
