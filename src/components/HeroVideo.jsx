74f9a49efc237d8db04cc3c0faee9200c7932596
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-04-08 20:06:39 +0200

    added more videos

--- src/components/HeroVideo.jsx
+++ src/components/HeroVideo.jsx
@@ -1,12 +1,24 @@
-import React from "react";
-import video from "../images/video.mp4";
+import React, { useState, useEffect } from "react";
+import video1 from "../images/video1.mp4";
+import video2 from "../images/video2.mp4";
+import video3 from "../images/video3.mp4";
 import "../css/HeroVideo.css";
 
 function HeroVideo() {
+  const [currentVideo, setCurrentVideo] = useState(0);
+  const videos = [video1, video2, video3];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentVideo((prevVideo) => (prevVideo + 1) % videos.length);
+    }, 30000); // Change video every 5 seconds (adjust as needed)
+    return () => clearInterval(interval);
+  }, [videos.length]);
+
   return (
     <div className="hero-video-container">
       <video
-        src={video}
+        src={videos[currentVideo]}
         autoPlay
         muted
         loop
--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import '../css/Review.css'; // Make sure to adjust this import if necessary
+import React from "react";
+import "../css/Review.css"; // Make sure to adjust this import if necessary
 
 const Review = () => {
   return (
     <div className="review-container">
       <div className="review-card">
-        <img src="https://images.pexels.com/photos/614484/pexels-photo-614484.jpeg?auto=compress&cs=tinysrgb&w=600" alt="nature-pic-CSSsnippets" className="customer-img" />
-        <h3 className="customer-name">Title</h3>
-        <p className="review-message">Hover over the card to see the effect.</p>
-        <div className="rating">
-          <span className="star">&#9733;</span>
-          <span className="star">&#9733;</span>
-          <span className="star">&#9733;</span>
-          <span className="star">&#9733;</span>
-          <span className="star">&#9733;</span>
-        </div>
+        <img
+          src="https://images.pexels.com/photos/614484/pexels-photo-614484.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt="nature-pic-CSSsnippets"
+          className="customer-img"
+        />
+        <h3 className="customer-name">⭐⭐⭐⭐⭐</h3>
+        <p className="review-message">This be the best website oyah i beg </p>
       </div>
     </div>
   );
-}
+};
 
 export default Review;
