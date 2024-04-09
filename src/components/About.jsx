660c9777947d509d690980d7ff8df2ee17234a2e
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-04-09 09:41:47 +0200

    section updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,15 +8,15 @@ function About() {
       <div className="text">
         <h1>Hear From Chi & Yamu!</h1>
         <p>
-          In the world of culinary arts, cooking is akin to weaving a tapestry
-          of flavors or composing a vibrant drumbeat. Just as each ingredient
+          In the world of culinary arts, baking is akin to weaving a tapestry of
+          flavors or composing a vibrant drumbeat. Just as each ingredient
           brings its own rhythm, it's the dance of blending them that creates
-          true magic. As esteemed chefs Yamukelwa and Chiamaka affirm, the only
+          true magic. As esteemed bakers Yamukelwa and Chiamaka affirm, the only
           obstacle is the fear of the unknown. However, with a spirit of
           exploration and a dash of courage, anyone can embark on a flavorful
           journey. With our wisdom and encouragement, we believe that every
-          aspiring cook has the power to unleash their inner chef and infuse
-          their dishes with the rich heritage of African cuisine
+          aspiring baker has the power to unleash their inner pastry chef and
+          infuse their creations with their unique touch.
         </p>
       </div>
       <div className="image">
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import '../css/Footer.css'; // Import the CSS file for styling
+import { FaInstagram, FaTwitter, FaFacebook, FaLinkedin } from 'react-icons/fa';
+import '../css/Footer.css'; // Import your CSS file for styling
 
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <p className="footer-text">&copy; 2024 ChiYamu. All rights reserved.</p>
+      <div className="footer-icons">
+        <a href="https://www.instagram.com/your_instagram" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
+        <a href="https://twitter.com/your_twitter" target="_blank" rel="noopener noreferrer">
+          <FaTwitter />
+        </a>
+        <a href="https://www.facebook.com/your_facebook" target="_blank" rel="noopener noreferrer">
+          <FaFacebook />
+        </a>
+        <a href="https://www.linkedin.com/in/your_linkedin" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
       </div>
+      <p className="footer-text">&copy; 2024 Chi & Yamu. All rights reserved.</p>
     </footer>
   );
 }
